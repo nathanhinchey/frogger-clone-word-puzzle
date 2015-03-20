f434ff0cfda09495b75de1559f07cda604a2f74b
Nathan Hinchey <nathanhinchey@gmail.com>
2015-03-20 16:19:48 -0400

    Add basicMove to frog movement

--- Assets/FrogController.cs
+++ Assets/FrogController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class FrogController : MonoBehaviour {
 
+	// Variables to be set in Inspector
 	public float travelTimeSeconds = 1.0f;
 	public float travelDistance = 1.0f;
 	public float xMax = 7f;
@@ -10,25 +11,31 @@ public class FrogController : MonoBehaviour {
 	public float yMax = 7f;
 	public float yMin = -6f;
 
+	// Private Variables
+	private Vector3 basicMove;
 	private Transform frog;
 	private Vector3 jumpFrom;
 	private Vector3 jumpTo;
-
+	private Vector3 activeMovement;
 	private GameObject gameController;
 	private InputController inputController;
-
 	private Vector3 movement = Vector3.zero;
 
 	private bool moving = false;
-
 	private float timeToTarget = 0f;
+
 	private Quaternion up = 		Quaternion.Euler (new Vector3 (0,	0,   0));
 	private Quaternion down = 	Quaternion.Euler (new Vector3 (0,	0, 180));
 	private Quaternion left = 	Quaternion.Euler (new Vector3 (0,	0,  90));
 	private Quaternion right = 	Quaternion.Euler (new Vector3 (0,	0, -90));
 
+	// Variables used by other scripts
+
 	public bool isMoving {
 		get {return moving;}
+	}
+	public Vector3 restingSpeed {
+		get {return basicMove;}
 	}
 
 	//PUBLIC METHODS
@@ -66,7 +73,7 @@ public class FrogController : MonoBehaviour {
 		gameController = GameObject.FindWithTag("GameController");
 		inputController = gameController.GetComponent<InputController>();
 		inputController.SetActiveFrog(this.gameObject);
-
+		basicMove = Vector3.zero;
 	}
 
 	bool IsInBounds(Vector3 location){
@@ -101,8 +108,15 @@ public class FrogController : MonoBehaviour {
 			frog.position = jumpTo;
 			moving = false;
 		}
-		if (moving)
+
+		//TODO: get rid of this conditional
+		if (moving){
 			frog.position = Vector3.Lerp
-				(jumpTo, jumpFrom, (timeToTarget - Time.time) / travelTimeSeconds );
+					(jumpTo, jumpFrom, (timeToTarget - Time.time) / travelTimeSeconds )
+					+ basicMove;
+		}
+		else {
+			frog.position += basicMove;
+		}
 	}
 }
---
+++ Assets/PingPong.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PingPong : MonoBehaviour {
+
+	public Vector3 startPosition = new Vector3 (-3,1,0);
+	public Vector3 endPosition = new Vector3 (3,1,0);
+	public float travelTimeSeconds = 2.0f;
+
+	private Vector3 pointA;
+	private Vector3 pointB;
+	private Vector3 temp;
+
+	private float arrivalTime;
+	private Transform thisThing;
+	private float switchTime;
+
+	// Use this for initialization
+	void Start () {
+		arrivalTime = travelTimeSeconds;
+		pointA = startPosition;
+		pointB = endPosition;
+		thisThing = gameObject.transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		thisThing.position = Vector3.Lerp(
+			pointB,
+			pointA,
+			(arrivalTime - Time.time) / travelTimeSeconds
+		);
+
+		if (arrivalTime - Time.time <= 0.01f){
+			temp = pointA;
+			pointA = pointB;
+			pointB = temp;
+			arrivalTime = Time.time + travelTimeSeconds;
+		}
+	}
+}
